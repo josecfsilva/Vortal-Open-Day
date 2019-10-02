ead6804840ff55d5d7c3a0ae59e57eac1d86e9b3
Miguel <mynameismang@gmail.com>
2019-10-02 11:50:00 +0100

    Redux init

---
+++ src/actions/index.js
@@ -0,0 +1,4 @@
+
+export const getLinks = () => ({
+    type: 'GET_LINKS'
+  })
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+
+const init_links = ["Task 1", "Task 2", "Task 3"]
+
+const links = (state={links: init_links}, action) =>{
+    switch (action.type) {
+        case 'GET_LINKS':
+            return state;
+    }
+}
